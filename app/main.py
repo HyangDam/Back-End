2bd3d1b4a628d1e1fb8df28a618751f2fa128457
lovekukwithhan <skylar4790@naver.com>
2026-09-02 19:20:31 +0900

    fix: allow frontend local CORS origin

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ app.include_router(auth_router.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:3000",
         "http://localhost:5173",
         "https://front-end-psi-ashen.vercel.app",
     ],
